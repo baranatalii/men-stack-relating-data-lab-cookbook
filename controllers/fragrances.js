627b99ced98b8112d7c605a781d1d13d4014f22f
Baranatalii <baranatalii@BARANs-MacBook-Air.local>
2026-01-19 16:02:41 -0500

    Built the Index Page Done

--- controllers/fragrances.js
+++ controllers/fragrances.js
@@ -2,9 +2,21 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const User = require("../models/user.js");
 
-// This is the Index route - it will show the vault later
-router.get("/", (req, res) => {
-  res.send("Welcome to the Fragrance Vault!");
+// Index Route - Renders the user's fragrance vault
+router.get("/", async (req, res) => {
+  try {
+    // Look up to the user by the ID in the URL
+    const currentUser = await User.findById(req.params.userId);
+
+    // Render the index.ejs file inside the views/fragrances folder
+    // We pass the fragrances to the view
+    res.render("fragrances/index.ejs", {
+      fragrances: currentUser.fragrances,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
