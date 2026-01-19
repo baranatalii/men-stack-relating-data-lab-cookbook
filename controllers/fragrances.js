b0109a8f50e562c266920957805be9357a1f3435
Baranatalii <baranatalii@BARANs-MacBook-Air.local>
2026-01-19 16:16:12 -0500

    Build The New Page

--- controllers/fragrances.js
+++ controllers/fragrances.js
@@ -2,17 +2,38 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const User = require("../models/user.js");
 
-// Index Route - Renders the user's fragrance vault
+// INDEX - GET /users/:userId/fragrances
 router.get("/", async (req, res) => {
   try {
-    // Look up to the user by the ID in the URL
     const currentUser = await User.findById(req.params.userId);
 
-    // Render the index.ejs file inside the views/fragrances folder
-    // We pass the fragrances to the view
     res.render("fragrances/index.ejs", {
-      fragrances: currentUser.fragrances,
+      fragrances: currentUser.fragrances || [],
     });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/new", (req, res) => {
+  res.render("fragrances/new.ejs");
+});
+
+// CREATE - POST /users/:userId/fragrances
+router.post("/", async (req, res) => {
+  try {
+    // 1. Find the user by the ID in the URL
+    const currentUser = await User.findById(req.params.userId);
+
+    // 2. Push the form data (req.body) into the user's fragrances array
+    currentUser.fragrances.push(req.body);
+
+    // 3. Save the changes to the user document in MongoDB
+    await currentUser.save();
+
+    // 4. Redirect the user back to their vault to see the new scent
+    res.redirect(`/users/${currentUser._id}/fragrances`);
   } catch (error) {
     console.log(error);
     res.redirect("/");
--- models/user.js
+++ models/user.js
@@ -1,8 +1,29 @@
 const mongoose = require("mongoose");
 
+// 1. Define the schema for the individual fragrance
+const fragranceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+});
+
+// 2. Define the User schema
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  // 3. Add the array of fragrances as an embedded subdocument
+  fragrances: [fragranceSchema], 
 });
 
 module.exports = mongoose.model("User", userSchema);
